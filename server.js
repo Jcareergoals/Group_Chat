711bccefc581fef9d865d05395b55981d85da835
Jose Chery <Jcareergoals@hotmail.com>
2016-06-26 21:07:08 -0400

    1. Created new refined socket events 2. Changed listening port to 3000 3. Added disconnect/login notification feature

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var session = require('express-session');
 var app = express(); 
 
-// set static, views and veiw engine
+// CONFIGURATIONS
 app.use(express.static(path.join(__dirname, '/static'))); 
 app.set('views', path.join(__dirname, '/views')); 
 app.set('view engine', 'ejs');
@@ -12,23 +12,28 @@ app.get('/', function(req, res){
 	res.render('index'); 
 });
 
-// set port for server to listen to 
-var server = app.listen(2222, function(){
-	console.log("Listening at port: 2222"); 
+var online = 0;
+// LISTENING PORT 
+var server = app.listen(3000, function(){
+	console.log("Listening at port: 3000"); 
 }); 
 
 var io = require("socket.io").listen(server);
-var messages = []; 
+
+
 io.sockets.on('connection', function(socket){
 	socket.on("new_message", function(data){
-		var new_message = {
-			name: data.user, 
-			message: data.message
-		}
-		messages.push(new_message); 
-		io.emit("updated", {messages: messages}); 
+		io.emit("new_message_update", data);
 	});
-	socket.on("update", function(){
-		socket.emit("updated", {messages: messages}); 
-	});  
+	socket.on("new_user", function(data){
+		socket.emit("welcome", data);
+		online += 1;   
+		io.emit("online", {online: online}); 
+		socket.broadcast.emit("newUserBroadcast", data);
+	}); 
+	socket.on('disconnect', function(data){
+		online -= 1; 
+		if(online < 0){online = 0;}
+		io.emit("disconnected", {online: online});
+	});
 }); 
